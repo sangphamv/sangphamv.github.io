0f1ef3d32876ec4cd901500f1d560e7dbaf1eb66
sangphamv <sangphamv.ee@gmail.com>
2025-03-30 21:41:55 +0700

    hhh

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sangphamv.github.io/', 
-    base: '/sangphamv.github.io/', 
+    base: '/', 
     output: 'static', 
 });
